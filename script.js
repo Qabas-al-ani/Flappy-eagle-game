c39d5034be4a004297cfabd4d7bce3aa8f79a788
Qabas <qabasalani1@gmail.com>
2022-04-17 20:48:08 -0400

    added the count points function that get the points counted for the user or the player

--- script.js
+++ script.js
@@ -35,3 +35,23 @@ setInterval(function () {
     counter = 0;
   }
 }, 10);
+
+// added the count points function that get the points counted for the user or the player
+function jump() {
+  jumping = 1;
+  let jumpCount = 0;
+  var jumpInterval = setInterval(function () {
+    var characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    if (characterTop > 6 && jumpCount < 15) {
+      character.style.top = characterTop - 5 + "px";
+    }
+    if (jumpCount > 20) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
+  }, 10);
+}
