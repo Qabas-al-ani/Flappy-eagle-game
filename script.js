d2a5e473db0fad78c3977f06237ca8c9a7ad6556
Qabas <qabasalani1@gmail.com>
2022-04-17 20:44:18 -0400

    set the interval for the animation jumps

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ hole.addEventListener("animationiteration", () => {
   hole.style.top = random + "px";
   counter++;
 });
+
+// set the interval for the animation jumps
+setInterval(function () {
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  if (jumping == 0) {
+    character.style.top = characterTop + 3 + "px";
+  }
+  var blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+  var cTop = -(500 - characterTop);
+  if (
+    characterTop > 480 ||
+    (blockLeft < 20 &&
+      blockLeft > -50 &&
+      (cTop < holeTop || cTop > holeTop + 130))
+  ) {
+    alert("Game over. Score: " + (counter - 1));
+    character.style.top = 100 + "px";
+    counter = 0;
+  }
+}, 10);
